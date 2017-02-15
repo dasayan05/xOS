ee051ba353c373b4db28d9bd55f5c45b06159cff
Ayan Das <dasayan05@hotmail.com>
2017-02-15 15:06:46 +0530

    stage1 and 2 has separate src folders and build targets

---
+++ boot/include/stage2/main.c
@@ -0,0 +1,5 @@
+void Stage2CMain()
+{
+	asm("cli");
+	asm("hlt");
+}
--- boot/stage2.asm
+++ boot/stage2.asm
@@ -1,15 +1,14 @@
 [BITS 16]		; It is still 16 bits
-[ORG 0x0]
 
 section .text	; Text (only) section
+
+extern Stage2CMain
 
 %define GENERAL_SEGMENT 0x07E0
 %define STACK_SEGMENT 0x7000
 
 StageTwoEntry:
 jmp StageTwo	; jump to the main code
-
-%include "print.inc"
 
 StageTwo:
 	; This is where stage2 actually starts
@@ -26,10 +25,7 @@ StageTwo:
 	sti
 	; setup segment registers
 
-	
+	call Stage2CMain
 
 	cli			; halt
 	hlt			; the system
-
-data_area:
-	Greeting: db "Welcome to Stage-2 bootloader",0
