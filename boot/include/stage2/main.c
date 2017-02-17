79a740f1db6e09defbcb7260db0b0b23570a74d2
Ayan Das <dasayan05@hotmail.com>
2017-02-17 23:35:50 +0530

    Hello World program
    A simple hello world stage2-bootloader

--- boot/include/stage2/main.c
+++ boot/include/stage2/main.c
@@ -1,5 +1,17 @@
+#include "bios/interrupts.h"
+
+void printString(char*);
+
 void Stage2CMain()
 {
-	asm("cli");
-	asm("hlt");
+	char str[12] = "Hello World";
+	printString(str);
+	halt();
+}
+
+void printString(char* str)
+{
+	int c = 0;
+	while (str[c++] != '\0')
+		printChar(str[c-1]);
 }
